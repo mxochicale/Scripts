0f8cddb7770b1dffcec0b3544f89e94877253948
Miguel Perez-Xochicale <perez.xochicale@gmail.com>
2015-12-11 17:46:21 +0000

    updading background image

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 Scripts directory is allocated to upload different source codes that are worthwhile 
 to use in the common life of a GNU/Linux user.
 
+Miguel Perez-Xochicale 
+
+
 
--- auto-overlay-images/auto-overlayimage.sh
+++ auto-overlay-images/auto-overlayimage.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 # Filename: auto-overlay.sh
-# Version: 0.1
-# Date 7th of March 2015
-# Author: Perez-Xochicale M. A.
+#
+# Author: Miguel Perez-Xochicale
 # 
 # To run the bashscript, you should change the permission using
 # chmod x+a auto-overlayimage.sh
 # Then, you are able to run the script
 #  ./auto-overlayimage.sh
 #
+#
+#
+# CHANGES
+#
+# Fri Dec 11 17:26:43 GMT 2015
+# adding background 01 version to the images files
+#
+#
+#
+# First Version: 0.1
+# Date 7th of March 2015
+
+
 # References: 
 # http://stackoverflow.com/questions/4802153/overlaying-two-images-with-automatic-resize-using-imagemagick
 # https://snipt.net/ardchoille42/bash-script-for-a-simple-html-image-gallery
-#
+
 
 for img in *.png; do
-	if [ "$img" = github_background_mxochicale.png ]; then 
+	if [ "$img" = github_background_mxochicale01.png ]; then 
 	echo 
 	else
         echo "Olverlaying:" $img
-	composite github_background_mxochicale.png $img overlayed-$img
+	composite github_background_mxochicale01.png $img overlayed-$img
 	fi
-
 done
-
-
 
 
 

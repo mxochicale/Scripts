9158897df52b05cbbdd921648507aae7a6aab0ba
Miguel Perez-Xochicale <perez.xochicale@gmail.com>
2016-07-13 18:59:20 +0100

    artificial data

--- R/ploting_data/example_00/README.md
+++ R/ploting_data/example_00/README.md
@@ -1,14 +1,18 @@
-Plotting values from data that is in different txt files
+Plotting values from data from different txt files in different paths
 ---
 
+1. Generate artificial data  
+data path contains a create_artificial_data.R script
+for that purpose.
 
+2. Read data, plot it and save plots  
 Run ploting_tempeparture_curves.R to create graphics on the plot directory.
-To change different temperatues you have to modify the following variable (TEMP <- "1220")
+To change different temperatues you have to modify the following variable (TEMPVAL <- "201")
 and run the R script again that creates a png file having the name of the temperature.
 
 
 TODO.
-* Add descriptions
+* Add descriptions for the code
 * Create a second version to automatise the plot creation for each temparature
 * Create a better plot using more functions from the xyplot
 
--- R/ploting_data/example_00/ploting_tempeparture_curves.R
+++ R/ploting_data/example_00/ploting_tempeparture_curves.R
@@ -8,32 +8,32 @@ if (!require("lattice")) install.packages("lattice")
 library(lattice)
 
 
-#-----------------------------------------------------------------------
+#################
 #Reading data file
 
 main_path <- getwd()
-TEMP <- "1220"
+TEMPVAL <- "202"
 
-data_path_TEMP <- paste(main_path,"/data/T",TEMP, sep="")
+data_path_TEMP <- paste(main_path,"/data/TEMP",TEMPVAL, sep="")
 setwd(data_path_TEMP)
 
 details = file.info(list.files(pattern="*.txt"))  #list all files with a specific extension
 files = rownames(details)
- 
+
 
 #################
 # Read txt files
-accum1<-read.table(files[1], header=FALSE)
-comp0<-read.table(files[2], header=FALSE)
-comp1<-read.table(files[3], header=FALSE)
-parpres0<-read.table(files[4], header=FALSE)
-purpres<-read.table(files[5], header=FALSE)
-res0<-read.table(files[6], header=FALSE)
-sub0<-read.table(files[7], header=FALSE)
-temp_c<-read.table(files[8], header=FALSE)
+a<-read.table(files[1], header=FALSE)
+b<-read.table(files[2], header=FALSE)
+c<-read.table(files[3], header=FALSE)
+d<-read.table(files[4], header=FALSE)
+e<-read.table(files[5], header=FALSE)
+F<-read.table(files[6], header=FALSE)
+g<-read.table(files[7], header=FALSE)
+h<-read.table(files[8], header=FALSE)
 
 #Concatenating the txt files into a Matrix Data (MD)
-MD <- cbind (accum1, comp0,comp1,parpres0,purpres,res0,sub0,temp_c)
+MD <- cbind (a,b,c,d,e,F,g,h)
 
 
 #############################################
@@ -47,21 +47,23 @@ if (file.exists(plots_path)){
   setwd(file.path(main_path, plots_path))
 }
 
-filenameplot <- paste("TEMP",TEMP,"_.png",sep="")
+#############################################
+# Plotting data and saving it as images
+filenameplot <- paste("TEMP_",TEMPVAL,".png",sep="")
 png(filenameplot, height=900, width=1500,bg="white")
 
 xyplot( t(MD[1]) + t(MD[2]) +t(MD[3]) +t(MD[4]) +t(MD[5]) +t(MD[6]) + t(MD[7]) ~ 1:dim(MD)[1],
          pch=1:7, col=1:7, type = c("l","g"), lwd=3,
-         main=list(label="Title", cex=3, fontfamily="Times"),
+         main=list(label=paste("Temp=", TEMPVAL, sep=""), cex=3, fontfamily="Times"),
          xlab=list(label="Iterations", cex=3, fontfamily="Times"),
          ylab=list(label="Values", cex=3, fontfamily="Times"),
          )
 
 
-dev.off() 
+dev.off()
 
 
 
-               
+
 
 setwd(main_path)
